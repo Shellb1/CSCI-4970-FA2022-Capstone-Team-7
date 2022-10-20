bcc835e53e4234a32ee698393476df4c21aeb830
chriskiihne <46977523+chriskiihne@users.noreply.github.com>
2022-10-20 14:34:56 -0500

    templateEditors added template editors 2,3,4 for different libraries

---
+++ pdfAnnotations-app/src/app/editor3/editor3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor3',
+  templateUrl: './editor3.component.html',
+  styleUrls: ['./editor3.component.scss']
+})
+export class Editor3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ pdfAnnotations-app/src/app/editor4/editor4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor4',
+  templateUrl: './editor4.component.html',
+  styleUrls: ['./editor4.component.scss']
+})
+export class Editor4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
