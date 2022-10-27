f320206dab9039c605e6a84326a8933af76d51ef
chriskiihne <46977523+chriskiihne@users.noreply.github.com>
2022-10-27 13:33:32 -0500

    templateEditors added a template component 2, 3, 4

--- pdfAnnotations-app/src/app/app-routing.module.ts
+++ pdfAnnotations-app/src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { EditorComponent } from './editor/editor.component';
 import { HomeComponent } from './home/home.component';
-
+import { Editor2Component } from './editor2/editor2.component';
+import { Editor3Component } from './editor3/editor3.component';
+import { Editor4Component } from './editor4/editor4.component';
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'editor', component: EditorComponent},
+  {path: 'editor2', component: Editor2Component},
+  {path: 'editor3', component: Editor3Component},
+  {path: 'editor4', component: Editor4Component},
   {path: '**', redirectTo: './editor/web/viewer.html', pathMatch: 'full'},
   {path: '', component: HomeComponent}
 ];
