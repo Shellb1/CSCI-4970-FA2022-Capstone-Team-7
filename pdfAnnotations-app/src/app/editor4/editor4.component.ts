a75c60872ff53e8454a4c3ec04aee2defa62f6f5
Brody Himmelberg <bhimmelberg@unomaha.edu>
2022-11-01 15:48:34 -0500

    pdf-lib code

--- pdfAnnotations-app/src/app/editor4/editor4.component.ts
+++ pdfAnnotations-app/src/app/editor4/editor4.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
 @Component({
   selector: 'app-editor',
   templateUrl: './editor4.component.html',
@@ -10,9 +11,23 @@ export class Editor4Component implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.createPdf();
   }
   goToPage(page: String) {
     this.router.navigateByUrl('/' + page);
   }
-
+  async function createPdf() {
+    const pdfDoc = await PDFDocument.create()
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+  
+    const page = pdfDoc.addPage()
+    const { width, height } = page.getSize()
+    const fontSize = 30
+    page.drawText('Creating PDFs in JavaScript is awesome!', {
+      x: 50,
+      y: height - 4 * fontSize,
+      size: fontSize,
+      font: timesRomanFont,
+      color: rgb(0, 0.53, 0.71),
+    })
 }
